d3daf41dcbb599ce13df27f816d097bf680e583e
tmmGodinho <tomas.mmg@gmail.com>
2024-03-14 23:38:34 +0000

    Refactored code into MojiClient class

--- main.py
+++ main.py
@@ -13,85 +13,12 @@ from moji_client import MojiClient
 #TODO inside of class, create a gen_message method that returns the message you want to send. It should use the class variables defined through your init logic
 #TODO call format from your event method that will trigger it
 #TODO run it cleanly from your main.py file
+#TODO implement datetime filter on message get
+#TODO refactor message get
+#TODO refactor string build
 
 load_dotenv()
 client = MojiClient()
-
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
-
-    # corre procedimento 
-    await go_count()
-    # channel = client.get_channel(297815778084782081)
-    # await channel.send("<@196422847818629120> you're next")
-
-async def go_count():
-    channel = client.get_channel(1214006594090565642)
-    how_many_mojis = 0
-    word = "💩"
-    moji_dict = {}
-    async for message in channel.history():
-        if word in message.content:
-            if message.author.id in moji_dict.keys():
-                moji_dict[message.author.id] += 1
-                how_many_mojis += 1
-            else:
-                print("i go here")
-                moji_dict[message.author.id] = 1
-                how_many_mojis += 1              
-    print(f"We have collectively 💩 {how_many_mojis} times. Great job.")
-    print(moji_dict)
-    
-    ordered_list = dict(sorted(moji_dict.items(), key=lambda item: item[1], reverse=True))
-    ranking = 'Hello. This is a test of the ranking feature\n'
-    place = 1
-    for key in ordered_list:
-        match place:
-            case 1:
-                ranking += f":first_place: <@{key}> > {ordered_list[key]}\n"
-            case 2:
-                ranking += f":second_place: <@{key}> > {ordered_list[key]}\n"
-            case 3:
-                ranking += f":third_place: <@{key}> > {ordered_list[key]}\n"
-            case _:
-                ranking += f"#{place}:<@{key}> > {ordered_list[key]}\n"
-        place += 1
-    print(ranking)
-    embed=discord.Embed(title="Test Embed", description=ranking, color=discord.Color.random())
-    # await message.channel.send(embed=embed)
-
-    
-    
-    
-
-
-
-# :first_place::second_place: :third_place: 
-
-#     >>> for key in likes:         
-# ...     print(key, "->", likes[key])
-
-
-@client.event
-async def on_message(message):
-    print(message)
-
-    # if message.author.name == "chibanga":
-    #     await message.reply("Sou capaz de responder a mensagens ;)")
-
-    if message.author.name == "ponei_lider":
-        attack_girao_prob = random.random()
-        print(attack_girao_prob)
-        if attack_girao_prob < .25:
-            await message.reply("Calado eras poeta")
-        
-
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
 
 #make this run from system variable instead of having the key here 
 #bot_key = os.environ['BOT_KEY']
--- moji_client.py
+++ moji_client.py
@@ -1,10 +1,66 @@
 import discord
+import random
 
 class MojiClient(discord.Client):
     
     def __init__(self):
         # TODO: GET EMOJI FROM .ENV
-        # self.emoji = "💩"
+        self.emoji = "💩"
         intents = discord.Intents.default()
         intents.message_content = True
         super().__init__(intents = intents)
+
+    # @client.event
+    async def on_ready(self):
+        print(f'We have logged in as {self.user}')
+
+        # corre procedimento 
+        await self.go_count()
+        # channel = client.get_channel(297815778084782081)
+        # await channel.send("<@196422847818629120> you're next")
+
+    async def go_count(self):
+        channel = self.get_channel(1214006594090565642)
+        how_many_mojis = 0
+        moji_dict = {}
+        async for message in channel.history():
+            if self.emoji in message.content:
+                if message.author.id in moji_dict.keys():
+                    moji_dict[message.author.id] += 1
+                    how_many_mojis += 1
+                else:
+                    print("i go here")
+                    moji_dict[message.author.id] = 1
+                    how_many_mojis += 1              
+        print(f"We have collectively 💩 {how_many_mojis} times. Great job.")
+        print(moji_dict)
+        
+        ordered_list = dict(sorted(moji_dict.items(), key=lambda item: item[1], reverse=True))
+        ranking = 'Hello. This is a test of the ranking feature\n'
+        place = 1
+        for key in ordered_list:
+            match place:
+                case 1:
+                    ranking += f":first_place: <@{key}> > {ordered_list[key]}\n"
+                case 2:
+                    ranking += f":second_place: <@{key}> > {ordered_list[key]}\n"
+                case 3:
+                    ranking += f":third_place: <@{key}> > {ordered_list[key]}\n"
+                case _:
+                    ranking += f"#{place}:<@{key}> > {ordered_list[key]}\n"
+            place += 1
+        print(ranking)
+        embed=discord.Embed(title="Test Embed", description=ranking, color=discord.Color.random())
+        # await message.channel.send(embed=embed)
+
+    async def on_message(self, message):
+        print(message)
+        if message.author.name == "ponei_lider":
+            attack_girao_prob = random.random()
+            print(attack_girao_prob)
+            if attack_girao_prob < .25:
+                await message.reply("Calado eras poeta")
+        if message.author == self.user:
+            return
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello!')    
