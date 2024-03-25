bd163666ce0ddad40dd23a310d09665c08332f87
tmmGodinho <tomas.mmg@gmail.com>
2024-03-25 22:28:19 +0000

    refactor last month parse

--- main.py
+++ main.py
@@ -3,17 +3,13 @@ import os
 from moji_client import MojiClient
 
 
-# IMPLEMENT GOODBYE I SLEEP MESSAGE
-# CTRL+/ para comentar blocos
-
-
-#TODO implement datetime filter on message get
+# TODO set exception for tie in ranking
+# TODO insert month variable into ranking message
 
 load_dotenv()
 emoji = os.getenv("MOJI")
 channel = int(os.getenv("CHANNEL"))
 client = MojiClient(emoji, channel)
-
 
 bot_key = os.getenv("BOT_KEY")
 client.run(bot_key)
--- moji_client.py
+++ moji_client.py
@@ -1,114 +1,89 @@
 import discord
-import random
 from datetime import datetime
-
+from dateutil.relativedelta import relativedelta
 
 
 class MojiClient(discord.Client):
-    moji_dict = {}
-    ranking = ''
     moji_channel = None
-
 
     def __init__(self, emoji, channel_id):
         self.emoji = emoji
         intents = discord.Intents.default()
         intents.message_content = True
-        super().__init__(intents = intents)
+        super().__init__(intents=intents)
         self.channel_id = channel_id
-        
 
     async def on_ready(self):
-        print(f'We have logged in as {self.user}')
         self.moji_channel = self.get_channel(self.channel_id)
         await self.go_count()
 
-    async def go_count(self, last_month = False):
-        await self.parse_moji()
-        self.go_rank()
-        ranking = self.make_string()
-        await self.go_post()
+    async def go_count(self, is_last_month=False):
+        start_ds, end_ds = MojiClient.gen_date_range(is_last_month)
+        moji_dict = await self.fill_ranking(start_ds, end_ds)
+        moji_dict = self.go_rank(moji_dict)
+        ranking_msg = self.make_string(moji_dict)
+        await self.go_post(ranking_msg)
 
-
-    async def parse_moji(self, last_month = False):
-        day_today = datetime.now()
-        print("datetime is :", day_today)
-        day_one = day_today.replace(day = 1, hour = 0, minute = 0, second = 0)
-        print("first day of the month", day_one)
-        if(last_month != False):
-            final_day = day_one.replace(month = final_day.month-1)
-            await self.fill_ranking(day_one, final_day=final_day)
-        else:
-            await self.fill_ranking(day_one)
-        print(self.moji_dict)
-
-    async def fill_ranking(self, day_one, final_day = False):
+    async def fill_ranking(self, start_ds, end_ds):
+        moji_dict = {}
         how_many_mojis = 0
-        if(final_day == False):
-            final_day = datetime.now()
-        else:
-            pass
-        async for message in self.moji_channel.history(after = day_one, before = final_day, limit = 10000):
+        async for message in self.moji_channel.history(
+            after=start_ds, before=end_ds, limit=10000
+        ):
             if self.emoji in message.content:
-                if message.author.id in self.moji_dict.keys():
-                    self.moji_dict[message.author.id] += 1
+                if message.author.id in moji_dict.keys():
+                    moji_dict[message.author.id] += 1
                 else:
-                    self.moji_dict[message.author.id] = 1
-                how_many_mojis += 1      
-        print(f"We have collectively {self.emoji} {how_many_mojis} times. Great job.")
+                    moji_dict[message.author.id] = 1
+                how_many_mojis += 1
+        return moji_dict
 
-    def go_rank(self):
-        self.moji_dict = dict(sorted(self.moji_dict.items(), key=lambda item: item[1], reverse=True))
-        
-
-    def make_string(self):
-        self.ranking = 'Hello. This is a test of the ranking feature\n'
+    def make_string(self, moji_dict):
+        if not moji_dict:
+            return f"There are no {self.emoji} to rank yet!"
         place = 1
-        for key in self.moji_dict:
+        ranking_msg = "Hello. These are the ranking results: \n"
+        for key in moji_dict:
             match place:
                 case 1:
-                    self.ranking += f":first_place: <@{key}> > {self.moji_dict[key]}\n"
+                    ranking_msg += f":first_place: <@{key}> > {moji_dict[key]}\n"
                 case 2:
-                    self.ranking += f":second_place: <@{key}> > {self.moji_dict[key]}\n"
+                    ranking_msg += f":second_place: <@{key}> > {moji_dict[key]}\n"
                 case 3:
-                    self.ranking += f":third_place: <@{key}> > {self.moji_dict[key]}\n"
+                    ranking_msg += f":third_place: <@{key}> > {moji_dict[key]}\n"
                 case _:
-                    self.ranking += f"#{place}:<@{key}> > {self.moji_dict[key]}\n"
+                    ranking_msg += f"#{place}:<@{key}> > {moji_dict[key]}\n"
             place += 1
-        print(self.ranking)
+        return ranking_msg
 
-
-    async def parse_last_month(self):
-        self.moji_dict = {}
-        day_today = datetime.now()
-        print("datetime is :", day_today)
-        day_one = day_today.replace(day = 1, hour = 0, minute = 0, second = 0)
-        print("first day of the month", day_one)
-        day_one_last_month = day_one.replace(month=day_one.month-1)
-        print("first day of the last month", day_one_last_month)
-        await self.fill_ranking(day_one_last_month, day_one)
-        print(self.moji_dict)
-        
-
-
-    async def go_post(self):
-        print(self.ranking)
-        #embed=discord.Embed(title="Test Embed", description=ranking, color=discord.Color.random())
-        #await self.moji_channel.send(embed=embed)
-
-    async def go_last_month(self):
-        await self.parse_last_month()
-        self.go_rank()
-        ranking = self.make_string()
-        await self.go_post()
-
-
+    async def go_post(self, ranking_msg):
+        embed = discord.Embed(
+            title=f"{self.emoji} Ranking",
+            description=ranking_msg,
+            color=discord.Color.random(),
+        )
+        await self.moji_channel.send(embed=embed)
 
     async def on_message(self, message):
         if message.author == self.user:
             return
-        if message.content.startswith('$hello'):
-            await message.channel.send('Hello!') 
-        if message.content.startswith('$lastmonth'):
-            await self.go_last_month() 
-            
+        if message.content.startswith("$hello"):
+            await message.channel.send("Hello!")
+        if message.content.startswith("$lastmonth"):
+            await self.go_count(True)
+
+    @staticmethod
+    def gen_date_range(is_last_month):
+        day_today = datetime.now()
+        day_one = day_today.replace(day=1, hour=0, minute=0, second=0)
+        if is_last_month:
+            start_ds = day_one - relativedelta(months=1)
+            end_ds = day_one
+        else:
+            start_ds = day_one
+            end_ds = day_today
+        return start_ds, end_ds
+
+    @staticmethod
+    def go_rank(moji_dict):
+        return dict(sorted(moji_dict.items(), key=lambda item: item[1], reverse=True))
