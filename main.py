552d50a6744a65611f72cceaf133450d49e14711
tmmGodinho <tomas.mmg@gmail.com>
2024-03-25 22:50:10 +0000

    small main fix

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from moji_client import MojiClient
 # TODO set exception for tie in ranking
 # TODO insert month variable into ranking message
 
-load_dotenv()
-emoji = os.getenv("MOJI")
-channel = int(os.getenv("CHANNEL"))
-client = MojiClient(emoji, channel)
-
-bot_key = os.getenv("BOT_KEY")
-client.run(bot_key)
+if __name__ == "__main__":
+    load_dotenv()
+    emoji = os.getenv("MOJI")
+    channel = int(os.getenv("CHANNEL"))
+    client = MojiClient(emoji, channel)
+    bot_key = os.getenv("BOT_KEY")
+    client.run(bot_key)
